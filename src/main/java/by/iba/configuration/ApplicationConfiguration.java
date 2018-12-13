57f3842f492908ed29f2bfbb4a6ead1e1894931d
ShakhraiR <shahrai.robert@gmail.com>
2018-12-13 19:14:59 +0300

    Added mail config, constants, properties;

--- src/main/java/by/iba/configuration/ApplicationConfiguration.java
+++ src/main/java/by/iba/configuration/ApplicationConfiguration.java
@@ -1,6 +1,7 @@
 package by.iba.configuration;
 
 import by.iba.configuration.database.mongo.MongoConfiguration;
+import by.iba.configuration.mail.MailConfiguration;
 import by.iba.configuration.rest.template.RestTemplateConfiguration;
 import by.iba.configuration.swagger.SwaggerConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,14 +9,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({MongoConfiguration.class, RestTemplateConfiguration.class, SwaggerConfiguration.class})
+@Import({MongoConfiguration.class, RestTemplateConfiguration.class, SwaggerConfiguration.class, MailConfiguration.class})
 public class ApplicationConfiguration {
     private MongoConfiguration mongoConfiguration;
     private RestTemplateConfiguration restTemplateConfiguration;
+    private SwaggerConfiguration swaggerConfiguration;
+    private MailConfiguration mailConfiguration;
 
     @Autowired
-    public ApplicationConfiguration(MongoConfiguration mongoConfiguration, RestTemplateConfiguration restTemplateConfiguration) {
+    public ApplicationConfiguration(MongoConfiguration mongoConfiguration, RestTemplateConfiguration restTemplateConfiguration, SwaggerConfiguration swaggerConfiguration, MailConfiguration mailConfiguration) {
         this.mongoConfiguration = mongoConfiguration;
         this.restTemplateConfiguration = restTemplateConfiguration;
+        this.swaggerConfiguration = swaggerConfiguration;
+        this.mailConfiguration = mailConfiguration;
     }
 }
