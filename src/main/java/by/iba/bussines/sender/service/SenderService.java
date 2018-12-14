f4fc3e3f843ee37d821614791945968969e43e34
ShakhraiR <shahrai.robert@gmail.com>
2018-12-14 14:17:17 +0300

    Small fix

--- src/main/java/by/iba/bussines/sender/service/SenderService.java
+++ src/main/java/by/iba/bussines/sender/service/SenderService.java
@@ -1,12 +1,4 @@
 package by.iba.bussines.sender.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
-
-@Service
-public class SenderService {
-
-    @Autowired
-    private JavaMailSender javaMailSender;
+public interface SenderService {
 }
---
+++ src/main/java/by/iba/bussines/sender/service/v1/SenderServiceImpl.java
@@ -0,0 +1,21 @@
+package by.iba.bussines.sender.service.v1;
+
+import by.iba.bussines.sender.service.SenderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import javax.mail.internet.MimeMessage;
+
+@Service
+public class SenderServiceImpl implements SenderService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public void sendMeetingToUser(String meetingId, String userEmail) {
+        //MimeMessage message;
+        //javaMailSender.send(message);
+
+    }
+}
