4b4479a26dc17642fa1a3777e06a208aa66b51eb
ShakhraiR <shahrai.robert@gmail.com>
2018-12-21 15:34:35 +0300

    Added wrapper definer test

--- src/test/java/by/iba/bussines/meeting/wrapper/definer/Data.java
+++ src/test/java/by/iba/bussines/meeting/wrapper/definer/Data.java
@@ -1,4 +1,23 @@
 package by.iba.bussines.meeting.wrapper.definer;
 
+import by.iba.bussines.calendar.factory.type.MeetingType;
+import by.iba.bussines.timeslot.model.TimeSlot;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Data {
+
+    private static final String START_DATE_FOR_SINGLE_EVENT = "12/01/2018 11:30:00";
+    private static final String END_DATE_FOR_SINGLE_EVENT = "32/01/2018 11:30:00";
+    public static final MeetingType MEETING_TYPE_FOR_SINGLE_EVENT = MeetingType.SINGLE;
+
+    public static List<TimeSlot> createTimeSlotsForSingleEvent() {
+        List<TimeSlot> timeSlots = new ArrayList<>();
+        TimeSlot timeSlot = new TimeSlot();
+        timeSlot.setStartDateTime(START_DATE_FOR_SINGLE_EVENT);
+        timeSlot.setEndDateTime(END_DATE_FOR_SINGLE_EVENT);
+        timeSlots.add(timeSlot);
+        return timeSlots;
+    }
 }
--- src/test/java/by/iba/bussines/meeting/wrapper/definer/MeetingWrapperDefinerTest.java
+++ src/test/java/by/iba/bussines/meeting/wrapper/definer/MeetingWrapperDefinerTest.java
@@ -1,16 +1,30 @@
 package by.iba.bussines.meeting.wrapper.definer;
 
+import by.iba.bussines.calendar.factory.type.MeetingType;
 import by.iba.bussines.meeting.model.Meeting;
+import by.iba.bussines.meeting.wrapper.model.single.SingleMeetingWrapper;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runner.Runner;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class MeetingWrapperDefinerTest {
 
+    @Autowired
+    private MeetingWrapperDefiner meetingWrapperDefiner;
 
     @Test
     public void defineMeetingWrapperTest() {
         //given
         Meeting meeting = new Meeting();
-        meeting.setTimeSlots();
+        meeting.setTimeSlots(Data.createTimeSlotsForSingleEvent());
+        //when
+        SingleMeetingWrapper singleMeetingWrapper = meetingWrapperDefiner.defineMeetingWrapper(meeting);
+        //then
+        MeetingType meetingType = singleMeetingWrapper.getMeetingType();
+        Assert.assertEquals(meetingType, Data.MEETING_TYPE_FOR_SINGLE_EVENT);
+        Assert.assertNotNull(singleMeetingWrapper.getSession());
 
 
     }
