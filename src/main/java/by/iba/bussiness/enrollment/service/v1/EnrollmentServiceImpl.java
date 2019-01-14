d4c247d0818509d9442a6353fbafa1f6a5cdc2a6
ShakhraiR <shahrai.robert@gmail.com>
2019-01-14 12:52:20 +0300

    Added complex and simple facades;

--- src/main/java/by/iba/bussiness/enrollment/service/v1/EnrollmentServiceImpl.java
+++ src/main/java/by/iba/bussiness/enrollment/service/v1/EnrollmentServiceImpl.java
@@ -1,29 +1,22 @@
 package by.iba.bussiness.enrollment.service.v1;
 
 import by.iba.bussiness.appointment.Appointment;
-import by.iba.bussiness.appointment.AppointmentCreator;
 import by.iba.bussiness.appointment.AppointmentInstaller;
-import by.iba.bussiness.appointment.repository.AppointmentRepository;
-import by.iba.bussiness.calendar.CalendarStatus;
-import by.iba.bussiness.calendar.creator.CalendarCreator;
-import by.iba.bussiness.calendar.creator.installer.CalendarAttendeesInstaller;
 import by.iba.bussiness.calendar.date.DateHelperDefiner;
 import by.iba.bussiness.calendar.date.model.DateHelper;
 import by.iba.bussiness.calendar.learner.Learner;
-import by.iba.bussiness.enrollment.Enrollment;
 import by.iba.bussiness.enrollment.EnrollmentLearnerStatus;
 import by.iba.bussiness.enrollment.EnrollmentsInstaller;
-import by.iba.bussiness.enrollment.repository.EnrollmentRepository;
 import by.iba.bussiness.enrollment.service.EnrollmentService;
-import by.iba.bussiness.enrollment.status.EnrollmentStatus;
+import by.iba.bussiness.facade.ComplexCalendarSenderFacade;
+import by.iba.bussiness.facade.SimpleCalendarSenderFacade;
 import by.iba.bussiness.invitation_template.InvitationTemplate;
 import by.iba.bussiness.invitation_template.service.InvitationTemplateService;
 import by.iba.bussiness.meeting.Meeting;
+import by.iba.bussiness.meeting.MeetingType;
 import by.iba.bussiness.meeting.service.MeetingService;
 import by.iba.bussiness.sender.MailSendingResponseStatus;
-import by.iba.bussiness.sender.MessageSender;
 import by.iba.exception.ServiceException;
-import net.fortuna.ical4j.model.Calendar;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,7 +25,6 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
-import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -41,16 +33,12 @@ import java.util.List;
 public class EnrollmentServiceImpl implements EnrollmentService {
     private static final Logger logger = LoggerFactory.getLogger(EnrollmentServiceImpl.class);
     private MeetingService meetingService;
-    private CalendarAttendeesInstaller calendarAttendeesInstaller;
-    private MessageSender messageSender;
     private InvitationTemplateService invitationTemplateService;
     private EnrollmentsInstaller enrollmentsInstaller;
-    private EnrollmentRepository enrollmentRepository;
     private AppointmentInstaller appointmentInstaller;
-    private CalendarCreator calendarCreator;
-    private AppointmentRepository appointmentRepository;
-    private AppointmentCreator appointmentCreator;
     private DateHelperDefiner dateHelperDefiner;
+    private ComplexCalendarSenderFacade complexCalendarSenderFacade;
+    private SimpleCalendarSenderFacade simpleCalendarSenderFacade;
 
     @Value("${enrollment_by_email_and_meeting_id_endpoint}")
     private String ENDPOINT_FIND_ENROLLMENT_BY_PARENT_ID_AND_EMAIL;
@@ -60,27 +48,19 @@ public class EnrollmentServiceImpl implements EnrollmentService {
 
     @Autowired
     public EnrollmentServiceImpl(MeetingService meetingService,
-                                 CalendarAttendeesInstaller calendarAttendeesInstaller,
-                                 MessageSender messageSender,
                                  InvitationTemplateService invitationTemplateService,
                                  EnrollmentsInstaller enrollmentsInstaller,
-                                 EnrollmentRepository enrollmentRepository,
                                  AppointmentInstaller appointmentInstaller,
-                                 CalendarCreator calendarCreator,
-                                 AppointmentRepository appointmentRepository,
-                                 AppointmentCreator appointmentCreator,
-                                 DateHelperDefiner dateHelperDefiner) {
+                                 DateHelperDefiner dateHelperDefiner,
+                                 ComplexCalendarSenderFacade complexCalendarSenderFacade,
+                                 SimpleCalendarSenderFacade simpleCalendarSenderFacade) {
         this.meetingService = meetingService;
-        this.calendarAttendeesInstaller = calendarAttendeesInstaller;
-        this.messageSender = messageSender;
         this.invitationTemplateService = invitationTemplateService;
         this.enrollmentsInstaller = enrollmentsInstaller;
-        this.enrollmentRepository = enrollmentRepository;
         this.appointmentInstaller = appointmentInstaller;
-        this.calendarCreator = calendarCreator;
-        this.appointmentRepository = appointmentRepository;
-        this.appointmentCreator = appointmentCreator;
         this.dateHelperDefiner = dateHelperDefiner;
+        this.complexCalendarSenderFacade = complexCalendarSenderFacade;
+        this.simpleCalendarSenderFacade = simpleCalendarSenderFacade;
     }
 
     @Override
@@ -110,33 +90,12 @@ public class EnrollmentServiceImpl implements EnrollmentService {
         }
         InvitationTemplate invitationTemplate = invitationTemplateService.getInvitationTemplateByCode(request, invitationTemplateKey);
         Appointment appointment = appointmentInstaller.installAppointment(meeting, invitationTemplate);
-        BigInteger bigIntegerMeetingId = new BigInteger(meetingId);
-        List<Enrollment> enrollmentList = enrollmentRepository.getAllByParentId(bigIntegerMeetingId);
         List<MailSendingResponseStatus> mailSendingResponseStatusList = new ArrayList<>();
         DateHelper dateHelper = dateHelperDefiner.defineDateHelper(appointment.getTimeSlots());
-        for (Enrollment enrollment : enrollmentList) {
-            if (CalendarStatus.CANCELLED.equals(enrollment.getCalendarStatus())
-                    && EnrollmentStatus.CANCELLED.equals(enrollment.getStatus())) {
-                MailSendingResponseStatus badMailSendingResponseStatus =
-                        new MailSendingResponseStatus(false, "User has cancelled status. ", enrollment.getUserEmail());
-                mailSendingResponseStatusList.add(badMailSendingResponseStatus);
-            } else {
-                Calendar calendar = calendarCreator.createCalendar(enrollment, appointment, dateHelper);
-                if (calendar == null) {
-                    MailSendingResponseStatus badMailSendingResponseStatus =
-                            new MailSendingResponseStatus(false, "User has already updated version. ", enrollment.getUserEmail());
-                    mailSendingResponseStatusList.add(badMailSendingResponseStatus);
-                    logger.info("Don't need to send message to " + enrollment.getUserEmail());
-                } else {
-                    calendarAttendeesInstaller.addAttendeeToCalendar(enrollment, calendar);
-                    MailSendingResponseStatus mailSendingResponseStatus = messageSender.sendCalendarToLearner(calendar);
-                    mailSendingResponseStatusList.add(mailSendingResponseStatus);
-                    if (mailSendingResponseStatus.isDelivered()) {
-                        enrollmentsInstaller.installEnrollmentCalendarFields(enrollment, appointment);
-                    }
-                }
-            }
+        if (dateHelper.getMeetingType().equals(MeetingType.RECURRENCE)) {
+            mailSendingResponseStatusList = simpleCalendarSenderFacade.sendCalendar(dateHelper, appointment);
+        } else {
+            mailSendingResponseStatusList = complexCalendarSenderFacade.sendCalendar(dateHelper, appointment);
         }
-        return mailSendingResponseStatusList;
     }
 }
